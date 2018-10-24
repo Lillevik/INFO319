7ec9a446765582cc6dd9145c2938eba3f7261b94
svimanet <esj002@uib.no>
2018-10-24 15:23:36 +0200

    simple py script for querying in command line

--- query.py
+++ query.py
@@ -17,21 +17,36 @@ def get_db():
     return None
  
  
-def query_db(conn):
+def query_db(conn, query):
     """
     Query all rows in the Tweet table
     :param conn: the Connection object
     """
     cur = conn.cursor()
-    cur.execute("SELECT * FROM Tweet")
-    rows = cur.fetchall()
+    rows = None
+    if query.lower() == "all":
+        cur.execute("SELECT * FROM Tweet")
+        rows = cur.fetchall()
+        
+    elif "last " in query.lower():
+        num = int(query.split(" ")[1])
+        q = "SELECT * FROM Tweet ORDER BY created_at LIMIT {};".format(num)
+        cur.execute(q)
+        rows = cur.fetchall()
+
+    else:
+        cur.execute(query)
+        rows = cur.fetchall()
+    
     for row in rows:
         print(row)
- 
- 
+
 def run():
     # create a database connection
     conn = get_db()
-    query_db(conn)
+    print("Table = 'Tweet'\nPremade queries: all, last <num>")
+    while True:
+        user_inp = input("Query> ")
+        query_db(conn, user_inp)
 
 run()
