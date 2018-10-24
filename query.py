ca0f8562e3eb23788b063707c38715341f08acae
svimanet <esj002@uib.no>
2018-10-24 17:15:30 +0200

    add failsafe and general info func

--- query.py
+++ query.py
@@ -23,30 +23,49 @@ def query_db(conn, query):
     :param conn: the Connection object
     """
     cur = conn.cursor()
-    rows = None
     if query.lower() == "all":
         cur.execute("SELECT * FROM Tweet")
         rows = cur.fetchall()
+        for row in rows:
+            print(row)
         
     elif "last " in query.lower():
         num = int(query.split(" ")[1])
         q = "SELECT * FROM Tweet ORDER BY created_at LIMIT {};".format(num)
         cur.execute(q)
         rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+    elif "info" in query.lower():
+        q1 = "SELECT count(Tweet.id) FROM Tweet;"
+        q2 = "SELECT count(Place.id) FROM Place;"
+        q3 = "SELECT count(User.id) FROM User;"
+        cur.execute(q1)
+        row1 = cur.fetchone()[0]
+        cur.execute(q2)
+        row2 = cur.fetchone()[0]
+        cur.execute(q3)
+        row3 = cur.fetchone()[0]
+        print("Tweet: {}\nPlace: {}\nUser: {}".format(row1, row2, row3))
 
     else:
         cur.execute(query)
         rows = cur.fetchall()
+        for row in rows:
+            print(row)
     
-    for row in rows:
-        print(row)
 
 def run():
     # create a database connection
     conn = get_db()
-    print("Table = 'Tweet'\nPremade queries: all, last <num>")
+    print("Table = 'Tweet', 'Place', 'User'\nPremade queries: all, last <num>")
     while True:
-        user_inp = input("Query> ")
-        query_db(conn, user_inp)
-
+        try:
+            user_inp = input("Query> ")
+            print("\n")
+            query_db(conn, user_inp)
+            print("\n")
+        except Exception as e:
+            pass
 run()
